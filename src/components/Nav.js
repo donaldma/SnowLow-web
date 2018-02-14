3ec5dcd5e4eae7205cebcb4407fde65eb4e3e427
donaldma <donald.ma@hotmail.ca>
2018-02-14 02:24:25 -0800

    add react router links (doesn't work yet)

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { userHasAuthenticated } from '../actions/index'
 import { signOutUser } from '../actions/awsActions'
 
@@ -14,10 +15,10 @@ class Nav extends Component {
   render() {
     return(
       <nav className='navbar navbar-light navbar-expand-md'>
-        <a className='navbar-brand' href='/'>
+        <Link to={'/'} className='navbar-brand'>
           <img src='/img/logo.png' alt='logo.png' className='logo-image' />
           <span className='align-middle'>SnowLow</span>
-        </a>
+        </Link>
         <button className='navbar-toggler navbar-toggler-right collapsed' type='button' data-toggle='collapse' data-target='#navigation'>
           <span> </span>
           <span> </span>
@@ -27,18 +28,21 @@ class Nav extends Component {
         <div className='collapse navbar-collapse ' id='navigation'>
           <ul className='nav navbar-nav mt-3 mt-md-0 ml-md-auto'>
             <li className='nav-item d-flex d-md-none'>
-              <a className='nav-link' href='/'>Home</a>
+              {/* <a className='nav-link' href='/'>Home</a> */}
+              <Link to={'/'} className='nav-link'>Home</Link>
             </li>
             {
               !this.props.isAuthenticated &&
               <li className='nav-item'>
-                <a className='nav-link' href='/login'>Log in</a>
+                {/* <a className='nav-link' href='/login'>Log in</a> */}
+                <Link to={'/login'} className='nav-link'>Log in</Link>
               </li>
             }
             {
               !this.props.isAuthenticated &&
               <li className='nav-item'>
-                <a className='nav-link' href='/signup'>Sign up</a>
+                {/* <a className='nav-link' href='/signup'>Sign up</a> */}
+                <Link to={'/signup'} className='nav-link'>Sign up</Link>
               </li>
             }
             {
